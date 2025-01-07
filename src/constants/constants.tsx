bea6b5437e7c4c9725b140916f84e0353b77fbc4
Bhavy <84842331+datsBhavy@users.noreply.github.com>
2025-01-07 22:01:35 +0530

    Update constants.tsx

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,3 +1,3 @@
 const devURL = 'http://localhost:3022'
-const stagingURL = 'http://awsurlhere.com'
+const stagingURL = 'https://api.block-wire.com'
 export const baseURL = window.location.hostname?.includes('localhost') ? devURL : stagingURL
